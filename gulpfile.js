065646bf2b7183679f85c5289b638873188bbbde
Kuro-98 <101947718+Kuro-98@users.noreply.github.com>
2024-02-18 22:23:58 -0600

    Actualización de estilos y dependencias en el proyecto fin de paginas internas

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,50 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
-function css(done) {
-    //paso 1: identificar el archivo que vamos a compilar
-    //paso 2: compilarla
-    //paso 3: guardar el css
-    src('./src/scss/app.scss')
-        .pipe(sass())
-        .pipe(postcss([autoprefixer()]))
-        .pipe(dest('build/css')); //le decimos cual archivo va a compilar
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 
-    done();
+function css(done) {
+	//paso 1: identificar el archivo que vamos a compilar
+	//paso 2: compilarla
+	//paso 3: guardar el css
+	src('./src/scss/app.scss')
+		.pipe(sourcemaps.init()) //ESTA LINEA VA ANTES DE COMPILAR
+		.pipe(sass())
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(sourcemaps.write('.')) //ESTA LINEA VA DESPUES DE COMPILAR
+		.pipe(dest('build/css')); //le decimos cual archivo va a compilar
+
+	done();
 }
 
 function imagenes() {
-    return src('./src/img/**/*')
-        .pipe(imagemin({ optimizationlevel: 3 }))
-        .pipe(dest('build/img'));
+	return src('./src/img/**/*')
+		.pipe(imagemin({ optimizationlevel: 3 }))
+		.pipe(dest('build/img'));
 }
 
 function versionWebp() {
-    const opciones = {
-        quality: 50,
-    };
-    return src('src/img/**/*.{png,jpg}').pipe(webp(opciones)).pipe(dest('build/img'));
+	const opciones = {
+		quality: 50,
+	};
+	return src('src/img/**/*.{png,jpg}').pipe(webp(opciones)).pipe(dest('build/img'));
 }
 
 function versionAvif() {
-    const opciones = {
-        quality: 50,
-    };
-    return src('src/img/**/*.{png,jpg}').pipe(avif(opciones)).pipe(dest('build/img'));
+	const opciones = {
+		quality: 50,
+	};
+	return src('src/img/**/*.{png,jpg}').pipe(avif(opciones)).pipe(dest('build/img'));
 }
 
 function dev() {
-    watch('src/scss/**/*.scss', css);
-    watch('./src/img/**/*', imagenes);
+	watch('src/scss/**/*.scss', css);
+	watch('./src/img/**/*', imagenes);
 }
 
 exports.css = css;
